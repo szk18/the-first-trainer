9e83e4b05165d4f1959cbed64a20e75c5c7202fd
Suzuki Kensuke <s1811349@s.tsukuba.ac.jp>
2020-09-06 10:19:34 +0900

    update: connect to mysql

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,22 @@ import { createExpressServer } from "routing-controllers";
 import { CharacterController } from "./controller/CharacterContorllers";
 import { GachaController } from "./controller/GachaControllers";
 import { UserController } from "./controller/UserController";
+import { User } from "./entity/User";
+import { createConnection } from "typeorm";
 
-const app = createExpressServer({
-  controllers: [UserController, GachaController, CharacterController],
-});
+createConnection()
+  .then(async (connection) => {
+    const app = createExpressServer({
+      controllers: [UserController, GachaController, CharacterController],
+    });
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
-});
+    const user = new User("suzuki", "hoge token");
+    const userRepository = connection.getRepository(User);
+    await userRepository.save(user);
+    console.log("user has beeb saved");
+
+    app.listen(3000, () => {
+      console.log("listening on port 3000");
+    });
+  })
+  .catch((error) => console.error(error));
